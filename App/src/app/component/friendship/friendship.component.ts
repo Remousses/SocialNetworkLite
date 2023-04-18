6f410212b8f8c76535b2bf94268f94240bb0d90c
remousses <remy91490@hotmail.fr>
2023-04-18 19:36:16 +0200

    WIP

--- App/src/app/component/friendship/friendship.component.ts
+++ App/src/app/component/friendship/friendship.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-friendship',
-  templateUrl: './friendship.component.html'
+  templateUrl: './friendship.component.html',
+  styleUrls: ['./friendship.component.scss']
 })
 export class FriendshipComponent {
 
--- App/src/app/component/news/news.component.ts
+++ App/src/app/component/news/news.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-news',
-  templateUrl: './news.component.html'
+  templateUrl: './news.component.html',
+  styleUrls: ['./news.component.scss']
 })
 export class NewsComponent {
 
